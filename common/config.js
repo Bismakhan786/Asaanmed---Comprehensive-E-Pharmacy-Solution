c71056194debf0858b53f802226b0471b817fbf1
Bismakhan786 <bismaashfaq009@gmail.com>
2023-03-07 12:13:36 +0500

    Mobile app ready to deploy 2

---
+++ common/config.js
@@ -0,0 +1,5 @@
+// export const API_URL = "https://frozen-taiga-15187.herokuapp.com/api/v1"
+// export const API_URL = "https://panicky-toad-sheath-dress.cyclic.app/api/v1"
+export const API_URL = "https://amused-peplum-boa.cyclic.app/api/v1"
+// export const API_URL = "https://asaanmed-new-backend.vercel.app/api/v1"
+// export const API_URL = "http://10.0.2.2:4000/api/v1"
